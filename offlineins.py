b38df2d2b601a5ddea4650f04410517fcc8fabf8
Kin1009 <124960159+Kin1009@users.noreply.github.com>
2023-07-13 19:40:58 +0700

    Add files via upload

--- offlineins.py
+++ offlineins.py
@@ -1,45 +1,53 @@
-import base64
-import os
-import shutil
-import zipfile
-import sys
+
+from base64 import b64decode, b64encode
+from shutil import make_archive, move
+from sys import argv
+from tkinter import Tk
+from tkinter.ttk import Label
+from zipfile import ZipFile
+from os.path import isfile, isdir, basename, splitext
+from os import remove
 def create_installer(input_path, output_path):
-    # Convert input to zip
     temp_zip_path = 'temp.zip'
-    if os.path.isfile(input_path):
-        with zipfile.ZipFile(temp_zip_path, 'w') as zipf:
-            zipf.write(input_path, os.path.basename(input_path))
-    elif os.path.isdir(input_path):
-        shutil.make_archive(temp_zip_path, 'zip', input_path)
+    if isfile(input_path):
+        with ZipFile(temp_zip_path, 'w') as zipf:
+            zipf.write(input_path, basename(input_path))
+    elif isdir(input_path):
+        make_archive("temp", 'zip', input_path)
     else:
         print('Invalid input path.')
         return
-    # Convert zip to base64
     with open(temp_zip_path, 'rb') as zip_file:
-        base64_data = base64.b64encode(zip_file.read()).decode('utf-8')
-    # Write base64 data to output file
+        base64_data = b64encode(zip_file.read()).decode('utf-8')
     with open(output_path, 'w') as output_file:
         output_file.write(base64_data)
-    # Clean up temporary zip file
-    os.remove(temp_zip_path)
+    remove(temp_zip_path)
+    win.destroy()
 def extract_installer(input_path, output_path):
-    # Read base64 data from input file
     with open(input_path, 'r') as input_file:
         base64_data = input_file.read()
-    # Convert base64 to zip
     temp_zip_path = 'temp.zip'
     with open(temp_zip_path, 'wb') as zip_file:
-        zip_file.write(base64.b64decode(base64_data))
-    # Extract zip to output path
+        zip_file.write(b64decode(base64_data))
     if output_path.endswith('.zip'):
-        shutil.move(temp_zip_path, output_path)
+        move(temp_zip_path, output_path)
     else:
-        with zipfile.ZipFile(temp_zip_path, 'r') as zip_ref:
+        with ZipFile(temp_zip_path, 'r') as zip_ref:
             zip_ref.extractall(output_path)
-    # Clean up temporary zip file
-    os.remove(temp_zip_path)
-if len(sys.argv) == 2:
-    if sys.argv[1].endswith(".ins"):
-        extract_installer(sys.argv[1], os.path.dirname(sys.argv[1]))
+    remove(temp_zip_path)
+    win.destroy()
+def a(): pass
+if len(argv) == 2:
+    win = Tk()
+    win.resizable(0, 0)
+    win.protocol("WM_DELETE_WINDOW", a)
+    if argv[1].endswith(".ins"):
+        win.title("Extracting...")
+        Label(win, text="Extracting...").grid(column=0, row=0)
+        win.after(1000, lambda: extract_installer(argv[1], argv[1][:-4]))
+        win.mainloop()
     else:
-        create_installer(sys.argv[1], os.path.splitext(sys.argv[1])[0] + ".ins")
+        win.title("Packing...")
+        Label(win, text="Packing...").grid(column=0, row=0)
+        win.after(1000, lambda: create_installer(argv[1], splitext(argv[1])[0] + ".ins"))
+        win.mainloop()
